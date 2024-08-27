702e78b54bf62395208a033982c12742ab775a6c
sumit8080 <summit@pariltech.com>
2024-08-28 01:01:39 +0400

    h

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ const database = require('./config/database');
 const app = express();
 const host = process.env.APP_HOST;
 const port = process.env.APP_PORT;
-const api_version = process.env.API_VERSION;
 
 app.use(cors());
 app.use(helmet());
